0925c339cc4198e736a53c8f77a88276b20ab783
ckm960411 <78003237+ckm960411@users.noreply.github.com>
2022-01-28 21:46:12 +0900

    msw 수정, 간단한 get요청 처리 완료

--- mocks/browser.ts
+++ mocks/browser.ts
@@ -1,4 +1,4 @@
-import { setupWorker, SetupWorkerApi } from "msw";
+import { setupWorker } from "msw";
 import { handlers } from './handlers'
 
-export const worker: SetupWorkerApi = setupWorker(...handlers)
+export const worker = setupWorker(...handlers)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import AppLayout from "components/AppLayout";
 import { createTheme, ThemeProvider } from "@mui/material";
+import initMockAPI from "mocks";
 
 const themeOptions = createTheme({
   palette: {
@@ -11,10 +12,9 @@ const themeOptions = createTheme({
   },
 });
 
-(async () => {
-  const { worker } = await import('mocks/browser');
-  worker.start();
-})();
+if (process.env.NODE_ENV === 'development') {
+  initMockAPI();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
